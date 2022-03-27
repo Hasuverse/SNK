aacaf4217e442e744c8616b7565196ed0d395210
Eduard Abdullin <eduard.abdullin42@gmail.com>
2022-03-28 00:39:06 +0300

    add: camera follow & characters prefabs

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if(target == null)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
