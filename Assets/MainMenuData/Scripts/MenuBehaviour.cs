0fb07aebf43db50561e34853065181fba87fd3b7
Vlad <iiwolfiiiiwolfiI@gmail.com>
2022-03-31 14:18:25 +0300

    add:mainmenu

---
+++ Assets/MainMenuData/Scripts/MenuBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1); // Загружает сцену с индексом этой +1. Индексы в buildsettings 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ObamaCam()
+    {
+        Debug.Log("Obungo moment"); // Я хочу чтобы разные менюшки были с разными бг и камера плавно каталась от одной менюшки к другой. Попозже попробую все это запилить.
+    }
+}
