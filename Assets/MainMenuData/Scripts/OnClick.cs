a1963de0811f7f92920d4016944692e407420d41
Vlad <iiwolfiiiiwolfiI@gmail.com>
2022-03-31 19:07:01 +0300

    edit:menubtns

---
+++ Assets/MainMenuData/Scripts/OnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClick : MonoBehaviour
+{   
+    public UnityEvent fun;
+
+    void OnMouseDown()
+    {
+        
+    }
+
+    void OnMouseOver() 
+    {
+           fun.Invoke();
+    }
+
+}
+
